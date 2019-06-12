afcc469fb72da2ffd70879b903ecb951ae392b2e
Damien Wheatley <damien.wheatley@harrisfederation.org.uk>
2019-06-12 14:36:24 +0100

    Changed the for() loop in findSmallestInteger to a ForEach loop in order to splice referenceArray correctly.

--- Selection.js
+++ Selection.js
@@ -1,21 +1,21 @@
 //Hardcoded temporarily just to get it working.
-let inputArray = [5,3,2,1,6,8,4,7,2,8,14,82];
+let inputArray = [5,3,2,1,6,8,4,7,2,8,82,14];
 
 console.log("UNSORTED: " + inputArray);
 
 //Will eventually put a timer function in here - which will run asynchronous to the "moveSmallestInteger" function.
 function selectionSort (inputArray) {
-    moveSmallestInteger(inputArray);        
+    moveSmallestInteger(inputArray);
 };
 
 function moveSmallestInteger (inputArray) {
     let referenceArray = inputArray.slice(0);
     let elementCount = -1;
     let inputArrayLength = inputArray.length;
-    while(referenceArray.length !== 0 && elementCount <= inputArrayLength - 1){
+    while(referenceArray.length !== 0 && elementCount < inputArrayLength){
         let smallestInteger = findSmallestInteger(referenceArray);
         for(i=0;i<=inputArray.length - 1;i++){
-            if(inputArray[i] === smallestInteger){
+            if(inputArray[i] === smallestInteger && elementCount < inputArrayLength){
                 elementCount++;
                 let integerToSwap = inputArray[i];
                 let integerBeingSwapped = inputArray[elementCount];
@@ -32,8 +32,9 @@ function findSmallestInteger (referenceArray) {
     //removes smallest integer from "referenceArray" after finding it
     //otherwise it would constantly find the same number over and over.
     referenceArray.forEach(function(element,elementIndex) {
-        if(element === smallestInteger){
+        if(element === smallestInteger){ //it's removing element 0, which contains 8 - which moves element 1 to element 0, but because it has already done element 0 it skips 8.
             referenceArray.splice(elementIndex,1);
+            console.log(`Removing ${element} at position ${elementIndex}`)
         };
     });
     return smallestInteger;
