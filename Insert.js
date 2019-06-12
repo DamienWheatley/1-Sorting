46e98187c82e84ee9ccdeda2fd517c561043e8e5
Damien Wheatley <damien.wheatley@harrisfederation.org.uk>
2019-06-12 09:51:59 +0100

    Changed the for() loop in findSmallestInteger to a ForEach loop in order to splice referenceArray correctly.

--- Insert.js
+++ Insert.js
@@ -1,2 +1,23 @@
 let inputArray = [5,3,2,1,6,8,4,7,2,8,14,82];
 
+console.log(inputArray.length);
+
+function insertSort (inputArray) {
+    let orderedArray = [];
+    let leftMostInteger = getLeftMostInteger(inputArray);
+
+}
+
+function moveInteger (integerToMove, orderedArray) {
+    var orderedArrayLength = orderedArray.length - 1;
+    if(integerToMove > orderedArray[orderedArrayLength]){
+
+    }
+};
+
+function getLeftMostInteger (inputArray) {
+    leftMostInteger = inputArray.shift();    
+    return leftMostInteger;
+};
+
+//insertSort(inputArray);
--- Selection.js
+++ Selection.js
@@ -3,7 +3,7 @@ let inputArray = [5,3,2,1,6,8,4,7,2,8,14,82];
 
 console.log("UNSORTED: " + inputArray);
 
-//will eventually put a timer function in here - which will run asynchronous to the "moveSmallestInteger" function.
+//Will eventually put a timer function in here - which will run asynchronous to the "moveSmallestInteger" function.
 function selectionSort (inputArray) {
     moveSmallestInteger(inputArray);        
 };
@@ -11,11 +11,12 @@ function selectionSort (inputArray) {
 function moveSmallestInteger (inputArray) {
     let referenceArray = inputArray.slice(0);
     let elementCount = -1;
-    while(referenceArray != 0){
+    let inputArrayLength = inputArray.length;
+    while(referenceArray.length !== 0 && elementCount <= inputArrayLength - 1){
         let smallestInteger = findSmallestInteger(referenceArray);
         for(i=0;i<=inputArray.length - 1;i++){
             if(inputArray[i] === smallestInteger){
-                elementCount++
+                elementCount++;
                 let integerToSwap = inputArray[i];
                 let integerBeingSwapped = inputArray[elementCount];
                 inputArray[elementCount] = integerToSwap;
@@ -30,12 +31,11 @@ function findSmallestInteger (referenceArray) {
     let smallestInteger = Math.min.apply( Math,referenceArray);
     //removes smallest integer from "referenceArray" after finding it
     //otherwise it would constantly find the same number over and over.
-    for(j=0;j<=referenceArray.length;j++){
-        if(referenceArray[j] === smallestInteger){
-            referenceArray.splice(j,1);
+    referenceArray.forEach(function(element,elementIndex) {
+        if(element === smallestInteger){
+            referenceArray.splice(elementIndex,1);
         };
-    };
-    console.log(smallestInteger);
+    });
     return smallestInteger;
 };
 
